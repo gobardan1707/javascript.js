37617142514504576bbe15e110aca5e0a40bb01c
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2023-11-29 08:56:26 +0000

    datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";// treate all the js code in the newer version 
+//alert(3+3) we use alert in the console of browser because their their i a inbult js engine in the browser 
+// we cant use alert in node 
+// always write the code based on the redability.
+
+/* few data types are:
+number => 2 to the power 53
+string =>""
+boolean=> true/false
+null=>standaloon value 
+undefined => type
+symbol =>unique
+bigint => used for bigger numbers 
+object 
+*/
+console.log(typeof Object)
+console.log(typeof undefined)
+console.log(typeof null)
+
+//type of null is object 
