12c2fea424635cb6396d4792b4a2e1b62f662b20
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2023-12-12 16:27:44 +0000

    conversion operation

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,31 @@
+let score ="33"
+let a= true 
+let b = null
+let c= undefined 
+let d= "33ew"
+
+console.log(typeof(score))
+let valueinnumber = Number(score) // to convert a number into a string we can use value = Number(--) or we can use Number.parseInt()
+console.log(typeof(valueinnumber))
+// 
+
+let isloggedIn = 1
+let booleanisloggedIn = Boolean(isloggedIn)// we can convert into other forms uing the same method .
+console.log(booleanisloggedIn) // 1=> true and 0=>false
+
+// 
+console.log(typeof score )
+/************ ******operations****************** */
+
+console.log(1+1)
+let str1 = 1;
+let str2 = 2;
+let str3 = str1 + str2 ;
+console.log( str1 + str2 );
+console.log(1+2);
+console.log("1"+2);
+console.log(1+5+"5");
+console.log(true)
+console.log(+true)
+console.log("")
+console.log(+"")
