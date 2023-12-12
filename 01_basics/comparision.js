a991858a2ab11376be0071d0a4f2a449500e042d
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2023-12-12 17:07:37 +0000

    comparisio n

---
+++ 01_basics/comparision.js
@@ -0,0 +1,14 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2<1);
+console.log("2">1);
+console.log("2"<1);
+// always try to compare same data types
+
+console.log(null>0);
+console.log(null==0);
+console.log(null<0);//comparision convert the null to the zero .
+// if we write undefined in place of null then it will give false as output
+
+//=== it also compare the values and the datatypes of the compatible items or values.
+
