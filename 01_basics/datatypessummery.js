0b23e5b08d22dca5c606a270fb150e46c38383d7
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2023-12-17 08:26:38 +0000

    data type

---
+++ 01_basics/datatypessummery.js
@@ -0,0 +1,15 @@
+// their are mainly types of datatype they are 
+//primitive and non primitive or reference 
+
+
+//primitive 
+
+// 7 types :sting,number,null,undefined,symbol,bigint
+
+//symbols are often used to create unique idenfier unlike others symbols are unique and immutable
+//symbols are typically used as the keys for the object properties when yoi want to ensure that the prperty names are unique .
+//javascript is dynamic launguage    
+
+//reference(non primitive )
+ // arrays,objects,functuions
+
