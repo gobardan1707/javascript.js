5659157401fccb109716f9c589e1ba2680db907b
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2024-02-19 05:56:29 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,22 @@
+const name = "gobardan "
+console.log(name)
+// the modern way to writing is
+console.log( `hi mu name is ${name.topcase} and i got a package of $200k`)
+//modern method for declaring strigs is
+const newname = new String('gobardan')
+console.log(newname )
+console.log(newname[5])
+console.log(newname.length)
+console.log(newname.charAt(5))
+console.log(newname.indexOf('a'))
+const hi= name.substring(0,4)
+console.log(    hi  )
+const hi1= 'gobardan'
+console.log(hi1)
+console.log(hi1.trim())
+console.log(hi1.slice(0,3))
+
+const hi2= ' na peru gobardan'
+console.log(hi2.replace('gobardan','sai'))
+console.log(hi2.includes('sai'))
+console.log(hi2.includes('gobardan'))
