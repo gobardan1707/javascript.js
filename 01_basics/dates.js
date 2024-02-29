ed8c7aeeb3d8fd7dd480d99ad804ca33f1fed86a
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2024-02-29 07:36:34 +0000

    dates and changes

---
+++ 01_basics/dates.js
@@ -0,0 +1,8 @@
+const mydate= new Date();
+console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString('en-IN'));
+console.log(mydate.toTimeString())
