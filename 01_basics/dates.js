812c4fe0611a4827a54c56677afa47508e97928b
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2024-02-29 08:12:33 +0000

    datestime

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,4 +1,4 @@
-const mydate= new Date();
+/*const mydate= new Date();
 console.log(mydate);
 console.log(mydate.toDateString());
 console.log(mydate.toISOString());
@@ -6,3 +6,11 @@ console.log(mydate.toJSON());
 console.log(mydate.toLocaleDateString());
 console.log(mydate.toLocaleString('en-IN'));
 console.log(mydate.toTimeString())
+
+const mydates= new Date(2023,0,3)
+console.log(mydates.toISOString())
+console.log(mydates.toDateString())*/
+const mynewdate=Date.now()
+console.log(mynewdate.toLocaleString())//in front of objects we use the cloed brackets to get access to the functions ...
+console.log(mynewdate.getMonth)
+console.log(mynewdate.toLocaleString('default',{weekday:"long"}))
