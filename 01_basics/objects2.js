72f747eee2babf238b642c1c53320e2bb6f88501
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2024-03-14 11:38:28 +0000

    objects2

---
+++ 01_basics/objects2.js
@@ -0,0 +1,43 @@
+//const tinder=new object()
+const tinder = {}
+tinder.id = "123abc"
+tinder.username= "durga"
+tinder.isLoggedIn=false
+//console.log(tinder)
+//creating objcts inside objects..
+const regularuser={
+    email: "g@gmail.com",
+    fullname:{
+usersfullname:{
+firstname:"kolli" ,
+lastname:"reddy"
+}
+    }
+
+}
+//console.log(regularuser)
+//console.log(regularuser.fullname?.usersfullname.firstname)
+const obj1={1:"a",2:"b"}
+const obj2= {3:"c",4:"d"}
+const obj3={obj1,obj2}
+const obj4=Object.assign(obj1,obj2)
+const obj5= Object.assign({},obj1,obj2)//better way of writing when their are too many objects
+const obj6={...obj1,...obj2} /// spread method we can use it in case 0f arrays and string also
+console.log(obj3)
+console.log(obj4)
+console.log(obj5)
+console.log(obj6)
+
+const users=(
+    {
+        id:"ufhewhfu",
+        email:"djsn@gamil.com"
+    },
+    {
+
+    },
+    {
+
+    }
+)
+users[1].email;
