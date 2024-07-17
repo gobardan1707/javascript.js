ea313425867f37dc8a3f130eb226d255c3b64320
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2024-07-17 16:58:39 +0000

    switch

---
+++ switch.js
@@ -0,0 +1,16 @@
+const month = 3
+switch (month) {
+    case  1:
+        console.log("this is jan" )
+        break;
+        
+        case  2:
+            console.log("this is feb" )
+            break;
+        case  3:
+            console.log("this is march" )
+            break;
+    default: 
+    console.log("this is invalid")
+        break;
+}
