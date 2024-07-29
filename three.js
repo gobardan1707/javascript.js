ae504ae584f0913a443537f9e4848317f93b6429
GOBARDAN <120883178+gobardan1707@users.noreply.github.com>
2024-07-29 06:18:18 +0000

    fg

--- three.js
+++ three.js
@@ -12,26 +12,6 @@ for (const greet  of greetings)
     
 }*/
 
-const value = [1,2,3,45,5]
-const total = value.reduce(function(acc,cum){
-    console.log(`the value of acc is ${acc} and the value of cum is ${cum}`)
-    return acc+cum ;
-},0)
-console.log(total)
+//
 
-shoppingcart = [
-    {
-        item : "chart",
-        price : 388
-    },
-    {
-        item : "paper",
-        price : 388
-    },
-    {
-        item : "pen",
-        price : 388
-    }
-]
-const hat = shoppingcart.reduce((acc,item)=>acc+item.price,0)
-console.log(hat)
+console.log("hello")
